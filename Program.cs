7f06f1e93fd983dfb9e5d86b435328a6a83342cc
Adriana <adrianadefatimamartins30@gmail.com>
2025-01-09 16:26:21 -0300

    Programa em C# de média de alunos

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class MediaAlunos
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Cálculo da Média dos Alunos ===");
+
+        Console.Write("Digite o número de notas a serem inseridas: ");
+        int quantidadeNotas = int.Parse(Console.ReadLine());
+
+        double soma = 0;
+
+        for (int i = 1; i <= quantidadeNotas; i++)
+        {
+            Console.Write($"Digite a nota {i}: ");
+            double nota = double.Parse(Console.ReadLine());
+            soma += nota;
+        }
+
+        double media = soma / quantidadeNotas;
+
+        Console.WriteLine($"\nA média das notas é: {media:F2}");
+
+        if (media >= 7)
+        {
+            Console.WriteLine("Aluno aprovado!");
+        }
+        else if (media >= 5)
+        {
+            Console.WriteLine("Aluno em recuperação.");
+        }
+        else
+        {
+            Console.WriteLine("Aluno reprovado.");
+        }
+    }
+}
